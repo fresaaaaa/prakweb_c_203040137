e64740e850bf8734a4f3d11a0e4ccb73f8c0491a
fresaaaaa <fresabayu0601@gmail.com>
2022-10-10 11:27:18 +0700

    menyelesaikan video 3 mvc

--- mvc/app/core/app.php
+++ mvc/app/core/app.php
@@ -4,6 +4,17 @@ class App
 {
   public function __construct()
   {
-    echo 'OK!';
+    $url = $this->parseURL();
+    var_dump($url);
   }
-}
+
+
+  public function parseURL()
+  {
+    if (isset($_GET['url']))
+      $url = rtrim($_GET['url'], '/');
+    $url = filter_var($url, FILTER_SANITIZE_URL);
+    $url = explode('/', $url);
+    return $url; // video 3 mvc sampai sini..
+  }
+} // video 2 mvc sampai sini..
